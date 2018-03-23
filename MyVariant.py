d2f4f2cb1f12c4ce30c094473e14f08adededd54
U-RESEARCH\cja001 <cja001@RESD7-J05GQ6C.CRII.ORG>
2018-03-23 09:10:23 -0400

    Added some ACMG classifications to MyVariant class.

--- MyVariant.py
+++ MyVariant.py
@@ -1,5 +1,8 @@
 import requests
 
+from MyBrowser import MyBrowser
+
+
 class MyVariant():
 
     def __init__(self, copath_no, chr, pos, ref, alt, gene, transcript_id, c_dot):
@@ -14,7 +17,170 @@ class MyVariant():
 
 
     def get_functional_predictions(self):
-        params = {'add-all-data': 1, 'format': 'json', "Authorization": "Token aF31n1tErNi#yVp%j5@j2dL7Pws#xTYSZ8&b64@e"}
-        r = requests.get('https://api.varsome.com/lookup/' + self.chr + "-" + self.pos + "-" + self.ref + "-" + self.alt, params=params)
-        print(r.url)
-        return r.json()
+
+        headers = {'Content-Type': 'application/json',
+                   'Authorization': 'Token aF31n1tErNi#yVp%j5@j2dL7Pws#xTYSZ8&b64@e'}
+        params = {'add-all-data': 1}
+        r = requests.get('https://api.varsome.com/lookup/' + self.chr + "-" + self.pos + "-" + self.ref + "-" + self.alt, params=params, headers=headers)
+
+        tolerated = 0
+        damaging = 0
+        predictions_available = 0
+
+        ## get raw predictions from VarSome API
+        try:
+            mutation_taster = r.json()['dbnsfp'][0]['mutationtaster_pred'][0]
+            if mutation_taster == 'D':
+                damaging += 1
+            elif mutation_taster == 'N':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            mutation_taster = None
+
+        try:
+            mutation_assessor = r.json()['dbnsfp'][0]['mutationassessor_pred'][0]
+            if mutation_assessor == 'D' or mutation_assessor == 'H':
+                damaging += 1
+                predictions_available += 1
+            elif mutation_assessor == 'N' or mutation_assessor == 'L':
+                tolerated += 1
+                predictions_available += 1
+        except:
+            mutation_assessor = None
+
+        try:
+            fathmm = r.json()['dbnsfp'][0]['fathmm_pred'][0]
+            if fathmm == 'D':
+                damaging += 1
+            elif fathmm == 'T':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            fathmm = None
+
+        try:
+            fathmmMKL = r.json()['dbnsfp'][0]['fathmm_mkl_coding_pred'][0]
+            if fathmmMKL == 'D':
+                damaging += 1
+            elif fathmmMKL == 'N':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            fathmmMKL = None
+
+        try:
+            metaSVM = r.json()['dbnsfp'][0]['metasvm_pred'][0]
+            if metaSVM == 'D':
+                damaging  += 1
+            elif metaSVM == 'T':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            metaSVM = None
+
+        try:
+            metalR = r.json()['dbnsfp'][0]['metalr_pred'][0]
+            if metalR == 'D':
+                damaging  += 1
+            elif metalR == 'T':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            metalR = None
+
+        try:
+            provean = r.json()['dbnsfp'][0]['provean_pred'][0]
+            if provean == 'D':
+                damaging += 1
+            elif provean == 'N':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            provean = None
+
+        try:
+            LRT = r.json()['dbnsfp'][0]['lrt_pred'][0]
+            if LRT == 'D':
+                damaging += 1
+            elif LRT == 'N':
+                tolerated += 1
+            predictions_available += 1
+        except:
+            LRT = None
+
+        try:
+            sift = r.json()['dbnsfp'][0]['sift_prediction']
+            if sift == 'Damaging':
+                damaging += 1
+                predictions_available += 1
+            elif sift == 'Tolerated':
+                tolerated += 1
+                predictions_available += 1
+        except:
+            sift = None
+
+        try:
+            gerp = r.json()['gerp'][0]['gerp_rs'][0]
+            if gerp > 2:
+                damaging += 1
+            else:
+                tolerated += 1
+            predictions_available += 1
+        except:
+            gerp = None
+
+        #print (tolerated, damaging, predictions_available)
+
+        if tolerated > 6:
+            return 'tolerated'
+        elif damaging > 6:
+            return 'damaging'
+        else:
+            return 'conflicting'
+
+        ##return [mutation_taster, mutation_assessor, fathmm, fathmmMKL, metaSVM, metalR, provean, LRT, sift, gerp]
+
+
+    def get_pp2(self):
+
+        b = MyBrowser(head=False)
+        b.clinvar_missense_nav(self.gene)
+
+        total_missense = b.browser.find_by_xpath('//*[@id="_molConseq"]/li/ul/li[2]/span').value
+        total_missense = int(total_missense[1:][:-1])
+
+        benign_missense = b.browser.find_by_xpath('//*[@id="_Properties"]/li/ul/li[2]/span').value
+        benign_missense = int(benign_missense[1:][:-1])
+
+        likely_benign_missense = b.browser.find_by_xpath('//*[@id="_Properties"]/li/ul/li[3]/span').value
+        likely_benign_missense = int(likely_benign_missense[1:][:-1])
+
+        if (likely_benign_missense + benign_missense) / total_missense < 0.1:
+            return True
+        else:
+            return False
+
+
+    def get_bp1(self):
+
+        b = MyBrowser(head=False)
+        b.clinvar_path_nav(self.gene)
+
+        frameshift = b.browser.find_by_xpath('//*[@id="_molConseq"]/li/ul/li[1]/span').value
+        frameshift = int(frameshift[1:][:-1])
+
+        missense = b.browser.find_by_xpath('//*[@id="_molConseq"]/li/ul/li[2]/span').value
+        missense = int(missense[1:][:-1])
+
+        nonsense = b.browser.find_by_xpath('//*[@id="_molConseq"]/li/ul/li[3]/span').value
+        nonsense = int(nonsense[1:][:-1])
+
+        splice_site = b.browser.find_by_xpath('//*[@id="_molConseq"]/li/ul/li[4]/span').value
+        splice_site = int(splice_site[1:][:-1])
+
+        if (frameshift + nonsense + splice_site) / (frameshift + missense + nonsense + splice_site) > 0.9:
+            return True
+        else:
+            return False
+        
--- screenshots.py
+++ screenshots.py
@@ -17,64 +17,74 @@ if __name__ == '__main__':
     # c_dot = sys.argv[8]
 
     copath_no = 'M18-10000'
-    chr = '1'
-    pos = '155209685'
-    ref = 'G'
-    alt = 'A'
-    gene = 'SCN5A'
+    chr = '17'
+    pos = '72281284'
+    ref = 'C'
+    alt = 'T'
+    gene = 'DNAI2'
     transcript_id = 'ENST00000414099'
     c_dot = 'c.5881A>T'
 
     variant = MyVariant(copath_no, chr, pos, ref, alt, gene, transcript_id, c_dot)
 
-    #print(variant.get_functional_predictions())
+    fxn_pred = variant.get_functional_predictions()
+    if fxn_pred == 'tolerated':
+        print('BP4')
+    elif fxn_pred == 'damaging':
+        print('PP3')
+
+    if variant.get_bp1() == True:
+        print('BP1')
+
+    if variant.get_pp2() == True:
+        print('PP2')
 
     patient_path = r'U:\ASHITA\screenshots\\' + variant.copath_no
     variant_path = variant.gene + '_' + variant.chr + '_' + variant.pos + '_' + variant.ref + '_' + variant.alt
     screenshot_path = patient_path + '\\' + variant_path
 
 
-    ## Make directories for patient and variant if they don't already exist
-    if not os.path.exists(patient_path):
-        os.makedirs(patient_path)
-    if not os.path.exists(screenshot_path):
-        os.makedirs(screenshot_path)
-
-    # Open browser and take screenshots
-    b = MyBrowser()
-
-    # gnomAD
-    b.gnomad_nav(variant.chr, variant.pos, variant.ref, variant.alt)
-    b.take_screenshot(screenshot_path, 'gnomAD.jpg', 350, 200, 1600, 900)
-
-    # dbNSFP
-    b.dbnsfp_nav(variant.chr, variant.pos, variant.ref, variant.alt)
-    b.take_screenshot(screenshot_path, 'dbNSFP.jpg', 270, 170, 670, 1000)
-
-    # GERP
-    b.gerp_nav(variant.chr, variant.pos, variant.ref, variant.alt)
-    b.take_screenshot(screenshot_path, 'GERP.jpg', 270, 170, 1170, 600)
-
-    ## ClinVar missense
-    b.clinvar_missense_nav(variant.gene)
-    b.take_screenshot(screenshot_path, 'clinvar_missense.jpg', 5, 290, 193, 886)
-
-    ## Unclick missense
-    b.browser.find_by_text('Missense').click()
-
-    ## ClinVar P/LP
-    b.clinvar_path_nav(variant.gene)
-    b.take_screenshot(screenshot_path, 'clinvar_plp.jpg', 5, 290, 193, 886)
-
-    ## ExAC
-    b.exac_nav(variant.gene)
-    b.take_screenshot(screenshot_path, 'exac.jpg', 36, 200, 1750, 500)
-
-
-    ## OMIM
-    coords = b.omim_nav(variant.gene)
-    b.take_screenshot(screenshot_path, 'OMIM.jpg', coords['xCoord'], coords['yCoord'], coords['xCoord'] + coords['width'], coords['yCoord'] + coords['height'])
-
-    # # HSF
-    # b.hsf_nav(variant.c_dot, variant.transcript_id)
-    # b.take_screenshot(screenshot_path, 'HSF.jpg', 0, 0, 2000, 2000)
+    # ## Make directories for patient and variant if they don't already exist
+    # if not os.path.exists(patient_path):
+    #     os.makedirs(patient_path)
+    # if not os.path.exists(screenshot_path):
+    #     os.makedirs(screenshot_path)
+    #
+    # # Open browser and take screenshots
+    # b = MyBrowser()
+    #
+    # # gnomAD
+    # b.gnomad_nav(variant.chr, variant.pos, variant.ref, variant.alt)
+    # b.take_screenshot(screenshot_path, 'gnomAD.jpg', 350, 200, 1600, 900)
+    #
+    # # dbNSFP
+    # b.dbnsfp_nav(variant.chr, variant.pos, variant.ref, variant.alt)
+    # b.take_screenshot(screenshot_path, 'dbNSFP.jpg', 270, 170, 670, 1000)
+    #
+    # # GERP
+    # b.gerp_nav(variant.chr, variant.pos, variant.ref, variant.alt)
+    # b.take_screenshot(screenshot_path, 'GERP.jpg', 270, 170, 1170, 600)
+    #
+    # ## ClinVar missense
+    # b.clinvar_missense_nav(variant.gene)
+    # b.take_screenshot(screenshot_path, 'clinvar_missense.jpg', 5, 290, 193, 886)
+    #
+    # ## Unclick missense
+    # b.browser.find_by_text('Missense').click()
+    #
+    # ## ClinVar P/LP
+    # b.clinvar_path_nav(variant.gene)
+    # b.take_screenshot(screenshot_path, 'clinvar_plp.jpg', 5, 290, 193, 886)
+    #
+    # ## ExAC
+    # b.exac_nav(variant.gene)
+    # b.take_screenshot(screenshot_path, 'exac.jpg', 36, 200, 1750, 500)
+    #
+    #
+    # ## OMIM
+    # coords = b.omim_nav(variant.gene)
+    # b.take_screenshot(screenshot_path, 'OMIM.jpg', coords['xCoord'], coords['yCoord'], coords['xCoord'] + coords['width'], coords['yCoord'] + coords['height'])
+    #
+    # # # HSF
+    # # b.hsf_nav(variant.c_dot, variant.transcript_id)
+    # # b.take_screenshot(screenshot_path, 'HSF.jpg', 0, 0, 2000, 2000)
